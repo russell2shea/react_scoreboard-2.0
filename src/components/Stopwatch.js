dbb084646cdaf6f6293b7086b05f60ffd7348af3
Russell Shea <russell2shea@gmail.com>
2019-03-12 20:12:07 -0400

    added start, stop, and reset logic to stopwatch

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -4,26 +4,55 @@ class Stopwatch extends Component {
 
 
 	state = {
-		isRunning: false
+		isRunning: false,
+		elapsedTime: 0, 
+		previousTime: 0,
 	};
 
+	// called by react when a component is inserted into the dom. 
+	componentDidMount(){
+		//console.log('the stopwatch mounted, lifecycle hook');
+		this.intervalId = setInterval( () => this.tick(), 100);
+	}
+
+	tick = () => {
+		if (this.state.isRunning){
+			const now = Date.now();
+			this.setState( prevState => ({
+				previousTime: now, 
+				elapsedTime: prevState.elapsedTime + (now - this.state.previousTime)
+			}));
+		}
+	}
+
 	handleStopwatch = () => {
-		this.setState({
-			isRunning: !this.state.isRunning
-		});
+		this.setState( prevState => ({
+			isRunning: !prevState.isRunning
+		}));
+		if (!this.state.isRunning){
+			this.setState({ previousTime: Date.now() });
+		}
+	}
+
+
+	handleRest = () => {
+		this.setState({ elapsedTime: 0 });
 	}
 
 	render(){
+
+		const seconds = Math.floor(this.state.elapsedTime / 1000)
+
 		return(
 			<div className="stopwatch">
 				<h2>Stopwatch</h2>
-				<span className="stopwatch-time">0</span>
-				<button onClick={
-					this.handleStopwatch
-				}>
+				<span className="stopwatch-time">
+					{ seconds }
+				</span>
+				<button onClick={this.handleStopwatch}>
 					{ this.state.isRunning ? 'Stop' : 'Start'}
 				</button>
-				<button>Reset</button>
+				<button onClick={this.handleRest}>Reset</button>
 			</div>
 		);
 	}
